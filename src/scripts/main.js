5140c091c32bdf65e70d06a74d0f5b22f4ebda84
unknown <marian.fedun3423@gmail.com>
2026-09-16 13:11:38 +0200

    added task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,25 @@
 'use strict';
 
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+wall.style.position = 'relative';
+spider.style.position = 'absolute';
+
 document.addEventListener('click', (e) => {
-  // write code here
+  const wallRect = wall.getBoundingClientRect();
+
+  const currentPosition = {
+    x: e.clientX - wallRect.left - wall.clientLeft,
+    y: e.clientY - wallRect.top - wall.clientTop,
+  };
+
+  let x = currentPosition.x - spider.offsetWidth / 2;
+  let y = currentPosition.y - spider.offsetHeight / 2;
+
+  x = Math.max(0, Math.min(x, wall.clientWidth - spider.offsetWidth));
+  y = Math.max(0, Math.min(y, wall.clientHeight - spider.offsetHeight));
+
+  spider.style.left = `${x}px`;
+  spider.style.top = `${y}px`;
 });
